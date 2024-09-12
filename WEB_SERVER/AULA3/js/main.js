545fc64627b97fc119e52e40f5c2f99b7dde6b80
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2024-09-12 20:55:48 +0100

    aula android e aula web server

--- WEB_SERVER/AULA3/js/main.js
+++ WEB_SERVER/AULA3/js/main.js
@@ -1,1 +1,19 @@
-let desserts = ['Bolo de Chocolate', 'Bolacha', 'Semifrio', 'pudim'];
+
+function shoppingList() {
+    let cart = [];
+    let text = '';
+
+    while (text !== "codigo") {
+        text = prompt("Indique um produto que deixa adicionar ao carrinho");
+        cart.push(text);
+    }
+    cart.pop()
+    console.log(cart);
+}
+
+function rant(text) {
+    for (let i = 1; i < 4; i++) {
+        console.log(i, text.toUpperCase());
+    }
+}
+
