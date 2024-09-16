3f2ff27800cfdaff6f1d948389af558a18daa3cf
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2024-09-16 15:05:42 +0100

    aula web server aula 16 Setembro

--- WEB_SERVER/AULA3/js/main.js
+++ WEB_SERVER/AULA3/js/main.js
@@ -1,3 +1,12 @@
+const fullNames = [
+    {first: 'Albus', last: 'Dumbledore'},
+    {first: 'Harry', last: 'Potter'},
+    {first: 'Hermione', last: 'Granger'},
+    {first: 'Ron', last: 'Weasley'},
+    {first: 'Rubeus', last: 'Hagrid'},
+    {first: 'Minerva', last: 'McGonagall'},
+    {first: 'Severus', last: 'Snape'}
+];
 
 function shoppingList() {
     let cart = [];
@@ -17,3 +26,19 @@ function rant(text) {
     }
 }
 
+function greet() {
+    console.log(fullNames)
+    var name = prompt("Indique o Nome de quem quer apresentar").toLowerCase()
+    var found = fullNames.find((element) => element.first.toLowerCase() === name);
+    console.log(`O primeiro ${found.first} ${found.last}`);
+}
+
+function validUserName(arrayNames) {
+    const validNames = arrayNames.filter(name => name.length < 10);
+    console.log(validNames);
+}
+
+function allEvens(arrayNumbers) {
+    const isValid = arrayNumbers.every(number => (number % 2) === 1);
+    console.log(isValid);
+}
